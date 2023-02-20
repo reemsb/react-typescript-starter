eb42898a643e3697fafd48ba7d1ff4a678df655d
reem sb <rimssboui@gmail.com>
2023-02-20 15:48:17 +0100

    add babel
    - install needed packages.
    - update webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.tsx",
     mode: "development",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.(ts|tsx)?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.json', ".tsx"]
     },
     plugins: [new HtmlWebpackPlugin({
       template: "public/index.html",
